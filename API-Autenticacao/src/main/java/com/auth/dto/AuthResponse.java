fa1f8933e315b38e2d99e418c517cf586095ac1d
Pradixx <dieguittosilva09@gmail.com>
2026-05-24 03:12:45 -0300

    feat: adiciona DTO AuthResponse com token JWT e dados do usuário

---
+++ API-Autenticacao/src/main/java/com/auth/dto/AuthResponse.java
@@ -0,0 +1,25 @@
+package com.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+
+    @Builder.Default
+    private String type = "Bearer";
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+}
