a05f334ff88cf60b0dc4a469b5bb45a9f084ff55
Pradixx <dieguittosilva09@gmail.com>
2026-05-24 03:33:11 -0300

    feat: adiciona estrutura base do AuthController

---
+++ API-Autenticacao/src/main/java/com/auth/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.auth.controller;
+
+import com.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+}
